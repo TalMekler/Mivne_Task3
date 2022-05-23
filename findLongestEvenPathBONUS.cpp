d3c213e063e97c071c7f93c3e71cf2cc6ba99096
Tal Mekler <Tal.mekler11@gmail.com>
2022-05-23 21:53:20 +0300

    FORMATTING

--- findLongestEvenPathBONUS.cpp
+++ findLongestEvenPathBONUS.cpp
@@ -7,19 +7,23 @@ int findLongestEvenPathBONUS(BinaryTreeNode *tree) {
     BinaryTreeNode *bnArr1[30], *bnArr2[30];
     int max = -1, cnt = -1, size1 = 0, size2 = 0;
     bnArr1[size1] = tree;
-    size1++; cnt++;
+    size1++;
+    cnt++;
     while (cnt >= 0) {
-        if(bnArr1[size1-1]->getLeft() != nullptr && (atoi(bnArr1[size1 - 1]->getLeft()->getValue()) % 2 == 0 && atoi(bnArr1[size1-1]->getLeft()->getValue()) != 0)) {
-            bnArr1[size1] = bnArr1[size1-1]->getLeft();
+        if (bnArr1[size1 - 1]->getLeft() != nullptr && (atoi(bnArr1[size1 - 1]->getLeft()->getValue()) % 2 == 0 &&
+                                                        atoi(bnArr1[size1 - 1]->getLeft()->getValue()) != 0)) {
+            bnArr1[size1] = bnArr1[size1 - 1]->getLeft();
             bnArr1[size1]->setValue("0");
             size1++;
             cnt++;
-        } else if (bnArr1[size1-1]->getRight() != nullptr&&(atoi(bnArr1[size1-1]->getRight()->getValue()) % 2 == 0 && atoi(bnArr1[size1-1]->getRight()->getValue()) != 0)) {
-            bnArr1[size1] = bnArr1[size1-1]->getRight();
+        } else if (bnArr1[size1 - 1]->getRight() != nullptr &&
+                   (atoi(bnArr1[size1 - 1]->getRight()->getValue()) % 2 == 0 &&
+                    atoi(bnArr1[size1 - 1]->getRight()->getValue()) != 0)) {
+            bnArr1[size1] = bnArr1[size1 - 1]->getRight();
             bnArr1[size1]->setValue("0");
             size1++;
             cnt++;
-        }else {
+        } else {
             for (int i = 0; i < size1 - 1; ++i) {
                 bnArr2[size2] = bnArr1[i];
                 size2++;
